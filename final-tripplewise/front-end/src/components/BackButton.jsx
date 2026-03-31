9c2c9f113160960f309498bd42ca226234cf695f
nafiszaman3 <nafiszaman3@gmail.com>
2026-03-30 22:11:53 -0400

    fixed startup page

--- final-tripplewise/front-end/src/components/BackButton.jsx
+++
@@ -1,9 +0,0 @@
-import { styles } from "../styles";
-
-export default function BackButton({ onClick }) {
-  return (
-    <button onClick={onClick} style={styles.backButton}>
-      ←
-    </button>
-  );
-}
--- final-tripplewise/front-end/src/components/NextButton.jsx
+++
@@ -1,9 +0,0 @@
-import { styles } from "../styles";
-
-export default function NextButton({ onClick }) {
-  return (
-    <button onClick={onClick} style={styles.nextButton}>
-      →
-    </button>
-  );
-}
